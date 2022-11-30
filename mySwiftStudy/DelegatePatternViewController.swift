d915f39e643eda348ca99fbc49b69186fd192b8e
chulyeon <guinness987@gmail.com>
2022-11-30 17:58:20 +0900

    주석 추가

---
+++ mySwiftStudy/DelegatePatternViewController.swift
@@ -0,0 +1,15 @@
+/**
+ 📆 2022-11-30
+    - 첫 생성
+    - Delegate Pattern : A 객체의 기능을 B 객체에 분배 (컴포넌트 분리랑 비슷?)
+    - 대표적으로 TableView의 리스트와 리스트 클릭 이벤트 (작업이 다양)
+ */
+
+import UIKit
+
+class DelegatePatternViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
